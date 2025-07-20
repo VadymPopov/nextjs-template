cdea3d72b3c92d6988489ab2682b41ab249ea806
VadymPopov <boycristal10@gmail.com>
2025-07-19 21:26:58 -0400

    feat: add prettier, type-check,lint-staged

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,121 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { fixupConfigRules, fixupPluginRules } from '@eslint/compat';
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettier from 'eslint-plugin-prettier';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+export default defineConfig([
+  {
+    extends: fixupConfigRules(
+      compat.extends(
+        'next',
+        'eslint:recommended',
+        'prettier',
+        'next/core-web-vitals',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        'plugin:prettier/recommended',
+        'plugin:react-hooks/recommended',
+      ),
+    ),
 
-export default eslintConfig;
+    plugins: {
+      prettier: fixupPluginRules(prettier),
+      '@typescript-eslint': fixupPluginRules(typescriptEslint),
+      react: fixupPluginRules(react),
+      'react-hooks': fixupPluginRules(reactHooks),
+      'simple-import-sort': simpleImportSort,
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+
+      parser: tsParser,
+      ecmaVersion: 2021,
+      sourceType: 'module',
+
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+
+    rules: {
+      'prefer-const': 'warn',
+      'no-var': 'warn',
+      'no-unused-vars': 'warn',
+      'object-shorthand': 'warn',
+      'quote-props': ['warn', 'as-needed'],
+
+      '@typescript-eslint/array-type': [
+        'warn',
+        {
+          default: 'array',
+        },
+      ],
+
+      '@typescript-eslint/consistent-type-assertions': [
+        'warn',
+        {
+          assertionStyle: 'as',
+          objectLiteralTypeAssertions: 'never',
+        },
+      ],
+
+      'react/jsx-fragments': ['warn', 'syntax'],
+
+      'react/jsx-filename-extension': [
+        'warn',
+        {
+          extensions: ['ts', 'tsx'],
+        },
+      ],
+
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      'prettier/prettier': 'warn',
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            ['^\\u0000'], // side effects
+            ['^react', '^next'], // react, next
+            ['^@?\\w'], // npm packages
+            ['^~'], // alias imports
+            ['^\\.'], // relative imports
+          ],
+        },
+      ],
+      'simple-import-sort/exports': 'error',
+    },
+    ignores: ['.next/', 'dist/', 'build/', 'node_modules/'],
+  },
+]);
